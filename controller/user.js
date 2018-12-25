ccc3157d907436ab8d6e6e3193f61aa641fd4d38
xiansili <865915497@qq.com>
2018-12-25 18:35:46 +0800

    koa

--- controller/user.js
+++ controller/user.js
@@ -4,17 +4,41 @@ const mongoose = require('mongoose');
 
 
 router.post('/registUser', async (ctx) => {
-    const User = mongoose.model('User');
-
-    let newUser = new User(ctx.request.body);
-    await newUser.save().then(()=>{
-        ctx.body = {
-            code:200,
-            message: '注册成功'
-        }
-    });
+    // const User = mongoose.model('User');
+    // let newUser = new User(ctx.request.body);
+    // await newUser.save().then(()=>{
+    //     ctx.body = {
+    //         code:200,
+    //         message: '注册成功'
+    //     }
+    // });
     console.log('先思力你快成功了');
     ctx.body = "先思力你快成功了";
 }); 
 
 module.exports = router;
+
+
+// const Router = require('koa-router');
+// let router = new Router();
+// const mongoose = require('mongoose');
+
+// router.post('/registUser', async (ctx) => {
+//     // 获取model
+//     const User = mongoose.model('User');
+//     // 接收post请求封装成user对象
+//     let newUser = new User(ctx.request.body);
+//     // 使用save保存用户信息
+//     await newUser.save().then(() => {
+//         ctx.body = {
+//             code: 200,
+//             message: '注册成功'
+//         };
+//     }).catch(err => {
+//         ctx.body = {
+//             code: 500,
+//             message: err
+//         };
+//     });
+// });
+// module.exports = router;
--- index.js
+++ index.js
@@ -13,14 +13,7 @@ const bodyParser = require('koa-bodyparser');
 app.use(bodyParser());
 
 
-app.use(async ctx =>{
-    ctx.body = '是我吗？'
-});
 
-app.listen(3000, () => {
- console.log( "开启");
-  
-});
 
 //加载路由
 const Router = require('koa-router');
@@ -34,7 +27,6 @@ app.use(router.allowedMethods());
 
 
 
-
 //先连接，再初始化，异步操作（async()=>{await}）
 const {connect , initSchemas} = require('./init.js');
 (async ()=>{
@@ -42,4 +34,11 @@ const {connect , initSchemas} = require('./init.js');
  initSchemas();
 })();
 
+app.use(async ctx =>{
+    ctx.body = '是我吗？'
+});
 
+app.listen(4000, () => {
+ console.log( "开启");
+  
+});
