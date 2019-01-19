472eccce9cdc012bcd3a9905626f55b3d7601492
xiansili <865915497@qq.com>
2019-01-19 21:06:58 +0800

    koa

--- controller/user.js
+++ controller/user.js
@@ -6,8 +6,6 @@ const db = "mongodb://localhost/pw";
 router.post("/registUser", async ctx => {
   // 获取model
 
-  
-  
   const User = mongoose.model("User");
   // 接收post请求封装成user对象
   let newUser = new User(ctx.request.body);
